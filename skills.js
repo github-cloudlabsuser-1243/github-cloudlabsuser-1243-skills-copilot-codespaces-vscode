cb8cfaf7320c83ed8a498ca52b36fadab6e8d6ef
github-cloudlabsuser-1243 <168001677+github-cloudlabsuser-1243@users.noreply.github.com>
2025-02-04 20:51:20 +0000

    files

---
+++ skills.js
@@ -0,0 +1,9 @@
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+// Driver code
+console.log(celsiusToFahrenheit(0));    // 32
+console.log(celsiusToFahrenheit(25));   // 77
+console.log(celsiusToFahrenheit(-40));  // -40
+console.log(celsiusToFahrenheit(100));  // 212
